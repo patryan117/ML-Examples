e89e07836cea8d680af43fd24828e4ccfdb019cc
Patrick Ryan <patr@bu.edu>
2018-09-07 10:46:09 -0400

    Multi Classifier
    Sucessfuly refactored out references to the test dataset

--- b) Evaluating_Multiple_Classification_Models_(Titanic_Dataset).py
+++ b) Evaluating_Multiple_Classification_Models_(Titanic_Dataset).py
@@ -14,6 +14,7 @@
 
 # TODO: Convert .py files to Jupyter notebook and replace on Github
 
+# TODO: fix chained indexing
 # TODO: Add plotly plots
 # TODO: Add ROC curve overlay for each classifier
 # TODO: Create two different bar charts for test and train data (verify that the sss is creating identical test samples)
@@ -61,100 +62,59 @@ from sklearn.neural_network import MLPClassifier
 
 
 
+##########################################################################################################
+# LOAD DATSSETS
+##########################################################################################################
 
 
 # load test and train datasets (from kaggles's built in features)
 train = pd.read_csv('datasets/train.csv', header = 0, dtype={'Age': np.float64})
 test  = pd.read_csv('datasets/test.csv' , header = 0, dtype={'Age': np.float64})
-full_data = [train, test]
+full_data = [train]
+dataset = train
+
+
 
 
 
 #ways to inspect the dataframes in pandas
-# print(type(train))
+# print(type(train));
 # print(train.info())
 # print(train.head())
 # print("\n")
 
-# # taking the Pclass and Survived categories from sample, and showing the mean
-print (train[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean())
-# print("\n")
-
-# # Taking the sex and survived categories from sample and presenting the mean
-# print (train[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean())
-# print("\n")
 
 
 ##########################################################################################################
 # FEATURE ENGINEERING
 ##########################################################################################################
 
-
+# Verification Printouts:
+# print (train[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean())
+# print (train[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean())
+# print (train[['Embarked', 'Survived']].groupby(['Embarked'], as_index=False).mean())
+# print(train[['CategoricalFare', 'Survived']].groupby(['CategoricalFare'], as_index=False).mean())
+# print(pd.crosstab(train['Title'], train['Sex']))  # create a cross table to view the frequency of each title within each gender category
+# print(train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())  # table showing how title matches to each title
 
 
 # Create family size feature
-for dataset in full_data:
-    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1 # siblings + parents + self
-# print (train[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean())
-# print("\n")
-
-
-# Create a dummy variable for if family member is alone
-for dataset in full_data:
-    dataset['IsAlone'] = 0
-    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
-# print (train[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean())
-# Single passengers were more likely to have survived
-
-# Fill NA's in the embarked category with the most common origin point (S: Southampton)
-for dataset in full_data:
-    dataset['Embarked'] = dataset['Embarked'].fillna('S')
-# print (train[['Embarked', 'Survived']].groupby(['Embarked'], as_index=False).mean())
-
-
-# Fill in NA's in the "Fare" column with the median price (14.4542)
-# Create new element in train dataset using pd.qcut()
-for dataset in full_data:
-    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
-train['CategoricalFare'] = pd.qcut(train['Fare'], 4)
-# print(train[['CategoricalFare', 'Survived']].groupby(['CategoricalFare'], as_index=False).mean())
-
-
-# TODO: fix chained indexing
+dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1 # siblings + parents + self
+dataset['IsAlone'] = 0  # Create a dummy variable for if family member is alone
+dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+dataset['Embarked'] = dataset['Embarked'].fillna('S') # Fill NA's in the embarked category with the most common origin point (S: Southampton)
+dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median()) # Fill in NA's in the "Fare" column with the median price (14.4542)
+dataset['CategoricalFare'] = pd.qcut(dataset['Fare'], 4)  # Create new element in train dataset using pd.qcut()
 
 # creating features in both the train and test datasets (which are contained in the list full_data
-for dataset in full_data:
 
-    # set the mean age to age_avg (29.6991)
-    age_avg = dataset['Age'].mean()
-
-    # set the std of age to age_std (14.1812)
-    age_std = dataset['Age'].std()
-
-    # set the number of null values in age to age_null_count
-    age_null_count = dataset['Age'].isnull().sum()
-
-    # make a random list of ages ranging within 1 sd from the mean, size equals null count
-    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size = age_null_count)
-
-    # Assign the random data list to the NA elements in the "Age" category:
-    dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list
-
-    # cast "Age" category to interger
-    dataset['Age'] = dataset['Age'].astype(int)
-
-# Create new feature of categorical age (only in the train dataset)
-
-train['CategoricalAge'] = pd.cut(train['Age'], 5)
-# print(train['CategoricalAge'])
-
-
-# print a table showing how categorical age influenced the survival outcome
-# print(train[['CategoricalAge', 'Survived']].groupby(['CategoricalAge'], as_index=False).mean())
-# print("\n")
-
-
-
+age_avg = dataset['Age'].mean() # set the mean age to age_avg (29.6991)
+age_std = dataset['Age'].std()  # set the std of age to age_std (14.1812)
+age_null_count = dataset['Age'].isnull().sum()  # set the number of null values in age to age_null_count
+age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size = age_null_count) # make a random list of ages ranging within 1 sd from the mean, size equals null count
+dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list # Assign the random data list to the NA elements in the "Age" category:
+dataset['Age'] = dataset['Age'].astype(int)  # cast "Age" category to interger
+dataset['CategoricalAge'] = pd.cut(dataset['Age'], 5)  # Create new feature of categorical age
 
 
 # create a new function to extract title from a string runs that match "xxx. "string
@@ -168,58 +128,54 @@ def get_title(name):
         return ""  # else, return an empty string
 
 
-for dataset in full_data:
-    dataset['Title'] = dataset['Name'].apply(get_title)
-
-# create a cross table to view the frequency of each title within each gender category
-# print(pd.crosstab(train['Title'], train['Sex']))
-
-for dataset in full_data:
-
-    # replace the rare terms with the word "rare" in the title category
-    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
- 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
-
-    # replace the misspelled titles
-    # correct title can be inferred by gender and table (e.g. Mlle and Mme both corespond to female gendered observations)
-    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
-    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
-    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+dataset['Title'] = dataset['Name'].apply(get_title)
 
 
-# table showing how title matches to each title
-# print(train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
+
+dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')     # replace the rare terms with the word "rare" in the title category
+dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')      # replace the misspelled titles   ( correct title can be inferred by gender and table (e.g. Mlle and Mme both corespond to female gendered observations))
+dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+
 
 
 # rename the sex feature with dummy variables (0 for female and 1 for male)
-for dataset in full_data:
 
-    # Mapping Sex
-    dataset['Sex'] = dataset['Sex'].map({'female': 0, 'male': 1}).astype(int)
+# Mapping Sex
+dataset['Sex'] = dataset['Sex'].map({'female': 0, 'male': 1}).astype(int)
 
-    # Mapping titles
-    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
-    dataset['Title'] = dataset['Title'].map(title_mapping)
-    dataset['Title'] = dataset['Title'].fillna(0)
+# Mapping titles
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+dataset['Title'] = dataset['Title'].map(title_mapping)
+dataset['Title'] = dataset['Title'].fillna(0)
 
 
     # Mapping Embarked
-    dataset['Embarked'] = dataset['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
+dataset['Embarked'] = dataset['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
 
 
     # Mapping Fare
-    dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
-    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
-    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
-    dataset.loc[dataset['Fare'] > 31, 'Fare'] = 3
-    dataset['Fare'] = dataset['Fare'].astype(int)
+dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
+dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
+dataset.loc[dataset['Fare'] > 31, 'Fare'] = 3
+dataset['Fare'] = dataset['Fare'].astype(int)
 
     # Mapping Age
-    dataset.loc[dataset['Age'] <= 16, 'Age'] = 0
-    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
-    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
-    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
-    dataset.loc[dataset['Age'] > 64, 'Age'] = 4
+dataset.loc[dataset['Age'] <= 16, 'Age'] = 0
+dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+dataset.loc[dataset['Age'] > 64, 'Age'] = 4
+
+
+
+
+##########################################################################################################
+# FEATURE SELECTION
+##########################################################################################################
+
 
 
 # Feature Selection
@@ -229,14 +185,14 @@ train = train.drop(drop_elements, axis=1)
 train = train.drop(['CategoricalAge', 'CategoricalFare'], axis=1)
 
 # print(train.values,"\n")  # once done, train.values is inaccessible
-print(type(train.values))
+# print(type(train.values))
 
 
 train = train.values  # needs to be recast to allow slicing
 
-print(type(train))
+# print(type(train))
 
-print(train, "\n")
+# print(train, "\n")  #TODO: double check that all columns are supposed to be intergers (0-4)
 
 
 
