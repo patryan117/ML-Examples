bfb34ee8e79bf3935b09e980ddcc7c116ea2fd76
Patrick Ryan <patr@bu.edu>
2018-09-07 11:43:27 -0400

    Multi_Classifier Titanic
    Added ROC test file and refactoring for simplification...

--- b) Evaluating_Multiple_Classification_Models_(Titanic_Dataset).py
+++ b) Evaluating_Multiple_Classification_Models_(Titanic_Dataset).py
@@ -18,12 +18,9 @@
 # TODO: Add plotly plots
 # TODO: Add ROC curve overlay for each classifier
 # TODO: Create two different bar charts for test and train data (verify that the sss is creating identical test samples)
-# TODO: Remove all references to the original test dataset (is there any reason to read it in in the begining?)
 # TODO: Classifier parameters can be retested here, but they should be auto-adjusted using a loop)
 # TODO: Add plotly printout to show the timeit runtime for each model (optomized at its highest setting)
 # TODO: Make comparison plot for each ML algorithm, with
-# TODO: Automate using a loop or generator (on seperating the ages into bins)
-# TODO: Update with more feature engineering options (auto-adjust binning)
 
 
 
@@ -61,27 +58,24 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 
 
+from itertools import cycle
+
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from scipy import interp
+
+
 
 ##########################################################################################################
-# LOAD DATSSETS
+# LOAD DATSSET
 ##########################################################################################################
 
+# open csv train file as a pandas array (splitting will take place after feature engineering)
+dataset = pd.read_csv('datasets/train.csv', header = 0, dtype={'Age': np.float64})
 
-# load test and train datasets (from kaggles's built in features)
-train = pd.read_csv('datasets/train.csv', header = 0, dtype={'Age': np.float64})
-test  = pd.read_csv('datasets/test.csv' , header = 0, dtype={'Age': np.float64})
-full_data = [train]
-dataset = train
-
-
-
-
-
-#ways to inspect the dataframes in pandas
-# print(type(train));
-# print(train.info())
-# print(train.head())
-# print("\n")
 
 
 
@@ -98,16 +92,15 @@ dataset = train
 # print(train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())  # table showing how title matches to each title
 
 
-# Create family size feature
+# Create features for "is alone", , fill NA's in fare and embark, and create categorical fare set
 dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1 # siblings + parents + self
 dataset['IsAlone'] = 0  # Create a dummy variable for if family member is alone
-dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1  # If family size ==1, assign is alone to 1
 dataset['Embarked'] = dataset['Embarked'].fillna('S') # Fill NA's in the embarked category with the most common origin point (S: Southampton)
-dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median()) # Fill in NA's in the "Fare" column with the median price (14.4542)
+dataset['Fare'] = dataset['Fare'].fillna(dataset['Fare'].median()) # Fill in NA's in the "Fare" column with the median price (14.4542)
 dataset['CategoricalFare'] = pd.qcut(dataset['Fare'], 4)  # Create new element in train dataset using pd.qcut()
 
 # creating features in both the train and test datasets (which are contained in the list full_data
-
 age_avg = dataset['Age'].mean() # set the mean age to age_avg (29.6991)
 age_std = dataset['Age'].std()  # set the std of age to age_std (14.1812)
 age_null_count = dataset['Age'].isnull().sum()  # set the number of null values in age to age_null_count
@@ -129,40 +122,37 @@ def get_title(name):
 
 
 dataset['Title'] = dataset['Name'].apply(get_title)
-
-
-
 dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')     # replace the rare terms with the word "rare" in the title category
-dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')      # replace the misspelled titles   ( correct title can be inferred by gender and table (e.g. Mlle and Mme both corespond to female gendered observations))
+dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')  # replace the misspelled titles   ( correct title can be confirmed via gender and table (e.g. Mlle and Mme both corespond to female gendered observations))
 dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
 dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
 
 
 
 
-# rename the sex feature with dummy variables (0 for female and 1 for male)
 
 # Mapping Sex
-dataset['Sex'] = dataset['Sex'].map({'female': 0, 'male': 1}).astype(int)
+dataset['Sex'] = dataset['Sex'].map({'female': 0, 'male': 1}).astype(int)  # rename the sex feature with dummy variables (0 for female and 1 for male)
+
 
 # Mapping titles
-title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}  # Mapping the title with a
 dataset['Title'] = dataset['Title'].map(title_mapping)
 dataset['Title'] = dataset['Title'].fillna(0)
 
 
-    # Mapping Embarked
+# Mapping Embarked
 dataset['Embarked'] = dataset['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
 
 
-    # Mapping Fare
+# Mapping Fare
 dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
 dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
 dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
 dataset.loc[dataset['Fare'] > 31, 'Fare'] = 3
-dataset['Fare'] = dataset['Fare'].astype(int)
 
-    # Mapping Age
+
+# Mapping Age
 dataset.loc[dataset['Age'] <= 16, 'Age'] = 0
 dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
 dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
@@ -178,23 +168,14 @@ dataset.loc[dataset['Age'] > 64, 'Age'] = 4
 
 
 
-# Feature Selection
-drop_elements = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp', 'Parch', 'FamilySize']
+# Feature Selection  #TODO: Consider showing correlation between each column and survival rate (to back up intuition)
 
-train = train.drop(drop_elements, axis=1)
-train = train.drop(['CategoricalAge', 'CategoricalFare'], axis=1)
-
-# print(train.values,"\n")  # once done, train.values is inaccessible
-# print(type(train.values))
-
-
-train = train.values  # needs to be recast to allow slicing
+drop_elements = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp', 'Parch', 'FamilySize', 'CategoricalAge', 'CategoricalFare']
+train = dataset.drop(drop_elements, axis=1)
+train = train.values  # needs to be recast to allow slicing  (once done, train.values is inaccessible)
 
 # print(type(train))
-
-# print(train, "\n")  #TODO: double check that all columns are supposed to be intergers (0-4)
-
-
+# print(train, "\n")
 
 
 
@@ -222,54 +203,40 @@ classifiers = [
        solver='lbfgs', tol=0.0001, validation_fraction=0.1, verbose=False,
        warm_start=False),
 
-    # TODO: Describe why the MLP keeps throwing a different score each time (paremeter-independent)
-
     ]
 
 
 
 log_cols = ["Classifier", "Accuracy"]
 log = pd.DataFrame(columns=log_cols)   # create an empty df to populate
+sss = StratifiedShuffleSplit(n_splits=10, test_size=0.1, random_state=0) #create the sss object later to be used to split ( note that it doesnt take the sample as a parameter)
 
 
-
-
-#create the sss object later to be used to split ( note that it doesnt take the sample as a parameter)
-sss = StratifiedShuffleSplit(n_splits=10, test_size=0.1, random_state=0)
-
-
-
-
-# Concention Note: (use for matrices and lower case letters are used for vectors)
+# Convention Note: (use upper case letters ("X"/"Y") for matrices and lower case letters ("x"/"y") for vectors)
 X = train[0:, 1:]   # Assign everything but the first column to a matrix called X
 y = train[0:, 0]    # Assign the first column from the training dataset (i.e. survical boolean) to a vector called y
 
 
 acc_dict = {}  # dictinary to hold the accuracy of each classifier
 
-print(sss.split(X, y))
 
-# Test dataset must be derived from the train file since we don't know the actual values of the train file.
-
-for train_index, test_index in sss.split(X, y):
+for train_index, test_index in sss.split(X, y):  # Test dataset must be derived from the train file since we don't know the actual values of the train file.
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
     for clf in classifiers:
         name = clf.__class__.__name__  # built-in name eg. (KNeighborsClassifier)
-        clf.fit(X_train, y_train)  # fit each classifier object
+        clf.fit(X_train, y_train)  # fit each classifier object (feeding the train matrix (X inputs) and solutions (y vector)
         train_predictions = clf.predict(X_test)  # predict with each classifier using the X_test files
         acc = accuracy_score(y_test, train_predictions) #temp value acc to hold the accuracy
-
-
-#TODO: Theres gotta be a better way to do this!
 
         if name in acc_dict:  # if built-in matches the dictionary name
             acc_dict[name] += acc  # add accuracy to names value (0 + acc)
         else:
             acc_dict[name] = acc  #else accuracy == 0 (right?)
 
-# print(acc_dict)  (why are the accuracy entries on a scale of 1 - 10?)
+
+print(acc_dict)
 
 for clf in acc_dict:  # clf = classifier name (KNeighborsClassifier, GaussianNB)
     acc_dict[clf] = acc_dict[clf] / 10.0
