245d859500a6135e3f9a1dfd31e512701fd4b339
Patrick Ryan <patr@bu.edu>
2018-10-03 16:50:43 -0400

    Update a) Simple_Linear_Regression_(Boston_Housing_Dataset).py

--- a) Simple_Linear_Regression_(Boston_Housing_Dataset).py
+++ a) Simple_Linear_Regression_(Boston_Housing_Dataset).py
@@ -122,49 +122,6 @@ data = [trace]
 fig= go.Figure(data=data, layout=layout)
 plotly.offline.plot(fig)
 
-
-#
-# l= []
-# y= []
-# data= pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv")
-# # Setting colors for plot.
-# N= 53
-# c= ['hsl('+str(h)+',50%'+',50%)' for h in np.linspace(0, 360, N)]
-#
-# for i in range(int(N)):
-#     y.append((2000+i))
-#     trace0= go.Scatter(
-#         x= data['Rank'],
-#         y= data['Population']+(i*1000000),
-#         mode= 'markers',
-#         marker= dict(size= 14,
-#                     line= dict(width=1),
-#                     color= c[i],
-#                     opacity= 0.3
-#                    ),name= y[i],
-#         text= data['State']) # The hover text goes here...
-#     l.append(trace0);
-#
-# layout= go.Layout(
-#     title= 'Stats of USA States',
-#     hovermode= 'closest',
-#     xaxis= dict(
-#         title= 'Population',
-#         ticklen= 5,
-#         zeroline= False,
-#         gridwidth= 2,
-#     ),
-#     yaxis=dict(
-#         title= 'Rank',
-#         ticklen= 5,
-#         gridwidth= 2,
-#     ),
-#     showlegend= False
-# )
-# fig= go.Figure(data=l, layout=layout)
-# plotly.offline.plot(fig)
-
-
 coef_array = (pd.DataFrame({'Name':lm.coef_,'Age':boston.feature_names}))
 
 
